cf1ebc8ffdd2265636c38136c2218e97a6e000ed
Thompson <thompson@spendableapp.com>
2018-07-23 19:31:24 -0500

    inital commit

---
+++ Firebase-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Firebase-Bridging-Header.h
+//  Firebase
+//
+//  Created by Thompson Fletcher on 7/23/18.
+//  Copyright © 2018 Thompson Fletcher. All rights reserved.
+//
+
+#ifndef Firebase_Bridging_Header_h
+#define Firebase_Bridging_Header_h
+
+#import "Firebase.h"
+
+#endif /* Firebase_Bridging_Header_h */
